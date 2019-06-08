b8b9ac24c0122f81ff731a777d63543526a19869
Kepi <kepi@igloonet.cz>
2019-06-08 13:25:06 +0200

    Clarify that license is GPL 3 - same as for Roundcube
    We don't need exception for skins and plugins as this is plugin itself.

--- show_gravatar.php
+++ show_gravatar.php
@@ -1,15 +1,31 @@
 <?php
-
 /**
  * Show gravatar for sender
  *
  * This plugin will show gravatar picture for message sender.
  *
+ * Copyright (C) 2009 Ondřej Kudlík https://kepi.cz
+ *
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
  * Enable the plugin in config/main.inc.php
  *
  * @version 0.2
- * @author Ondra 'Kepi' Kudlik
- * @website http://kepi.cz/
+ * @author Ondra Kudlík (Kepi)
+ * @website https://kepi.cz/
  */
 
 class show_gravatar extends rcube_plugin
